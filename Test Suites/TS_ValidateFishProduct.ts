636125cc8f585695b17f4aaec48e5b28ec4ba498
mohan-jayabalan <mohanstc@gmail.com>
2024-09-29 12:18:22 +0530

    ValidateFishCommit

---
+++ Test Suites/TS_ValidateFishProduct.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_ValidateFishProduct</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b8d686af-0cbf-4828-a8fc-02b2537974a4</testSuiteGuid>
+   <testCaseLink>
+      <guid>860e80f8-1e16-4cf7-bf71-63fc508aa59c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_ValidateFishProduct</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
